4bed519515cf6648f008c5eb923a9765f5bcaf87
Sergio Semedi <sergio.semedi@gmail.com>
2019-03-16 20:58:19 +0100

    added configuration file

--- main.go
+++ main.go
@@ -2,11 +2,47 @@ package main
 
 import (
 	d "github.com/semedi/epfiot/driver"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
 
-func main() {
+type conf struct {
+	Host string `yaml:"host"`
+	Auth string `yaml:"auth"`
+}
 
-	driver := d.Driver{Controller: d.New_kvm("qemu:///system")}
+func (c *conf) get() *conf {
+
+	yamlFile, err := ioutil.ReadFile("epfiot.conf")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	return c
+}
+
+func (c conf) uri() string {
+	var u string
+
+	if c.Host == "localhost" {
+		u = "qemu:///system"
+	} else {
+		u = "qemu+" + c.Auth + "://" + c.Host + "/system"
+	}
+
+	return u
+}
+
+func main() {
+	var config conf
+	config.get()
+
+	driver := d.Driver{Controller: d.New_kvm(config.uri())}
 
 	driver.Start()
 }
