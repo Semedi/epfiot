f97edf62e9654f9477947a8ec6be9d6c75ddd8e0
Sergio Semedi <sergio.semedi@gmail.com>
2019-03-16 21:26:50 +0100

    Driver creation through config file

--- driver/driver.go
+++ driver/driver.go
@@ -1,5 +1,11 @@
 package driver
 
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
+
 type Controller interface {
 	Init()
 	Create()
@@ -10,9 +16,51 @@ type Controller interface {
 type Driver struct {
 	Controller
 }
+type conf struct {
+	Host   string `yaml:"host"`
+	Auth   string `yaml:"auth"`
+	Driver string `yaml:"driver"`
+}
+
+func (c *conf) get() *conf {
+
+	yamlFile, err := ioutil.ReadFile("epfiot.conf")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	return c
+}
+
+func (c conf) uri() string {
+	var u string
+
+	if c.Host == "localhost" {
+		u = "qemu:///system"
+	} else {
+		u = "qemu+" + c.Auth + "://" + c.Host + "/system"
+	}
+
+	return u
+}
+
+func (d *Driver) Init() {
+	var config conf
+	config.get()
+
+	switch config.Driver {
+	case "kvm":
+		d.Controller = New_kvm(config.uri())
+	default:
+		log.Fatalf("Unrecognized Driver")
+	}
+}
 
 func (d *Driver) Start() {
-	d.Controller.Init()
 	d.Controller.Create()
 	d.Controller.List()
 	d.Controller.Close()
--- main.go
+++ main.go
@@ -2,47 +2,11 @@ package main
 
 import (
 	d "github.com/semedi/epfiot/driver"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
-	"log"
 )
 
-type conf struct {
-	Host string `yaml:"host"`
-	Auth string `yaml:"auth"`
-}
-
-func (c *conf) get() *conf {
-
-	yamlFile, err := ioutil.ReadFile("epfiot.conf")
-	if err != nil {
-		log.Printf("yamlFile.Get err   #%v ", err)
-	}
-	err = yaml.Unmarshal(yamlFile, c)
-	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
-	}
-
-	return c
-}
-
-func (c conf) uri() string {
-	var u string
-
-	if c.Host == "localhost" {
-		u = "qemu:///system"
-	} else {
-		u = "qemu+" + c.Auth + "://" + c.Host + "/system"
-	}
-
-	return u
-}
-
 func main() {
-	var config conf
-	config.get()
+	driver := new(d.Driver)
 
-	driver := d.Driver{Controller: d.New_kvm(config.uri())}
-
+	driver.Init()
 	driver.Start()
 }
